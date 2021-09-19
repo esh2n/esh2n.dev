eba42685cb36a38f5dfa76f2696e7ec673551b04
Shunya Endo <shunyaendoh.bz@gmail.com>
2021-09-19 20:56:33 +0900

    feat: add lit

---
+++ src/defs/wc.d.ts
@@ -0,0 +1,14 @@
+/// <reference types="react-scripts" />
+
+declare namespace JSX {
+  interface IntrinsicElements {
+    'mwc-button': {
+      raised?: boolean;
+      children?: React.ReactChild;
+      onClick?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+    };
+    'my-element': {
+      name?: string;
+    };
+  }
+}
