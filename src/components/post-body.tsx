6b81b869086c2e724c59fdc8861e2f4151dd104e
Shunya Endo <shunyaendoh.bz@gmail.com>
2021-09-23 22:37:30 +0900

    chore: improve style

--- src/components/post-body.tsx
+++ src/components/post-body.tsx
@@ -2,22 +2,32 @@ import React, { useEffect } from 'react';
 import styled from '@emotion/styled';
 interface Props {
   html: string;
+  coverImage: string;
 }
 const StyledPostBodyWrapper = styled.article`
+  grid-area: post-body;
   background-color: #f5f5f5;
   border-radius: 20px;
-  filter: drop-shadow(10px 10px 10px rgba(0, 0, 0, 0.6));
-  width: calc(100% - 330px);
-  @media (max-width: 992px) {
-    width: 100%;
+  filter: drop-shadow(10px 10px 10px rgba(0, 0, 0, 0.1));
+  width: 100%;
+  display: block;
+  div {
+    text-align: center;
+  }
+  img {
+    width: 75%;
+    padding-top: 40px;
   }
 `;
-const PostBody = ({ html }: Props) => {
+const PostBody = ({ html, coverImage }: Props) => {
   useEffect(() => {
     import('@okra-ui/string-to-html');
   }, []);
   return (
     <StyledPostBodyWrapper>
+      <div>
+        <img src={coverImage} />
+      </div>
       <string-to-html stringifiedHTML={html} />
     </StyledPostBodyWrapper>
   );
